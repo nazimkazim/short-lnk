8d8a235c6b46ad4f1d31da174b9ef507add49567
Your Name <nazik_111@mail.ru>
2018-09-14 17:11:03 +0500

    add creating publications and subscriptions

--- imports/api/links.js
+++ imports/api/links.js
@@ -1,3 +1,12 @@
+import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 
 export const Links = new Mongo.Collection('links');
+
+if (Meteor.isServer) {
+  Meteor.publish('links', () => {
+    const url = 'gulmir';
+    return Links.find({ url: url });
+  });
+}
+Meteor.publish;
